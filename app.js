eb1ec52c03a36901835c501930baa29ad5388e2e
Rio Edwards <rioredwards@gmail.com>
2023-07-21 14:27:04 -0700

    Replace first and second operand with better names: target adn stored operand + start add operation

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 const displayEl = document.getElementById("display");
 const numberBtns = document.querySelectorAll(".number");
 const clearBtn = document.getElementById("clear");
+const addBtn = document.getElementById("add");
+const equalsBtn = document.getElementById("equals");
 
 let displayValue = 0;
-let firstOperand = null;
-let secondOperand = null; // Update when an operator is clicked (And then another number is clicked)
+let targetOperand = null;
+let storedOperand = null; // Update when an operator is clicked (And then another number is clicked)
 let operator = null; // Update when an operator is clicked
 
 numberBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     const number = e.target.innerText;
-    if (firstOperand !== null) {
-      firstOperand = firstOperand * 10 + parseInt(number);
-    } else {
-      firstOperand = parseInt(number);
+    if (number === ".") {
+      // TODO: Handle decimal
     }
+
+    if (targetOperand !== null) {
+      targetOperand = targetOperand * 10 + parseInt(number);
+    } else {
+      targetOperand = parseInt(number);
+    }
+
     updateDisplay();
   });
+});
+
+addBtn.addEventListener("click", (e) => {
+  operator = "+";
+  storedOperand = targetOperand;
+  targetOperand = null;
+  updateDisplay();
 });
 
 clearBtn.addEventListener("click", clearAll);
 
 function clearAll() {
   displayValue = 0;
-  firstOperand = null;
-  secondOperand = null;
+  targetOperand = null;
+  storedOperand = null;
   operator = null;
   updateDisplay();
 }
 
 function updateDisplay() {
-  displayValue = operator ? secondOperand : firstOperand;
+  displayValue = targetOperand;
   displayEl.innerText = displayValue || 0;
+  log();
 }
 
 updateDisplay();
+
+function log() {
+  console.clear();
+  console.log("displayValue: ", displayValue);
+  console.log("targetOperand: ", targetOperand);
+  console.log("storedOperand: ", storedOperand);
+  console.log("operator: ", operator);
+}
