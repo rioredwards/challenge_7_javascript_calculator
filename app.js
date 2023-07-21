39304dd7141c897997975f2900d58f42a179c8ae
Rio Edwards <rioredwards@gmail.com>
2023-07-21 14:11:46 -0700

    Start writing out logic... Buttons can be pressed and their number displayed + clear action working

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Hello World");
+const displayEl = document.getElementById("display");
+const numberBtns = document.querySelectorAll(".number");
+const clearBtn = document.getElementById("clear");
+
+let displayValue = 0;
+let firstOperand = null;
+let secondOperand = null; // Update when an operator is clicked (And then another number is clicked)
+let operator = null; // Update when an operator is clicked
+
+numberBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const number = e.target.innerText;
+    firstOperand = parseInt(number);
+    updateDisplay();
+  });
+});
+
+clearBtn.addEventListener("click", clearAll);
+
+function clearAll() {
+  displayValue = 0;
+  firstOperand = null;
+  secondOperand = null;
+  operator = null;
+  updateDisplay();
+}
+
+function updateDisplay() {
+  displayValue = operator ? secondOperand : firstOperand;
+  displayEl.innerText = displayValue || 0;
+}
+
+updateDisplay();
